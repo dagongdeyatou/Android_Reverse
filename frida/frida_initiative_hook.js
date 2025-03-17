dbd2c24eccaf9d2b673ca57e5a5895235caf0cfd
wuye <wuye.wzy@alibaba-inc.com>
2025-03-17 21:25:14 +0800

    initiative

---
+++ frida/frida_initiative_hook.js
@@ -0,0 +1,16 @@
+function exec_frida_initiative_hook(){
+    Java.perform(function(){
+        Java.choose("com.xiaoeryu.lesson4_4.MainActivity",{
+            onMatch:function(instance){
+                console.log("found instance: ", instance)
+                console.log("found instance: ", instance.secret())
+            },onComplete:function(){}
+        })
+    })
+}
+
+function main(){
+    exec_frida_initiative_hook()
+}
+
+setImmediate(main)
--- frida/frida_initiative_static_hook.js
+++ frida/frida_initiative_static_hook.js
@@ -1,6 +1,8 @@
 function exec_frida_initiative_static_hook(){
-    var result = Java.use("com.xiaoeryu.lesson4_4.MainActivity").secret2();
-    console.log(result);
+    Java.perform(function(){
+        var result = Java.use("com.xiaoeryu.lesson4_4.MainActivity").secret2();
+        console.log(result);
+    })
 }
 
 function main(){
